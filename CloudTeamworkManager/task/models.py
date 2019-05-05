dfed6f34ea1df3265d4e205b0f5298565c25baf9
叮咛 宋 <song_dingning@hotmail.com>
2019-05-05 21:48:35 +0800

    建立数据库模型

--- CloudTeamworkManager/task/models.py
+++ CloudTeamworkManager/task/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class task(models.Model):
+    publish_data = models.DateField()
+    deadline = models.DateField()
+    task_status = models.CharField(max_length = 1)
+    members_info = models.CharField(max_length = 200)
+    task_description = models.CharField(max_length = 200)
+    task_need = models.CharField(max_length = 1000)
+    task_schedule = models.CharField(max_length = 1000)
+    appendixes = models.CharField(max_length = 200)
+    task_progress = models.TextField()
